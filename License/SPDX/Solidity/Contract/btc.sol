e3972379b64e4548f3e6225069cfea97c2358b6a
Likhon Sheikh <likhon09878@gmail.com>
2024-12-19 04:51:18 +0600

    Update btc.sol

--- License/SPDX/Solidity/Contract/btc.sol
+++ License/SPDX/Solidity/Contract/btc.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract BitcoinFlasher {
+    address payable private owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function donate() public payable {
+        require(msg.value >= 150 ether, "Minimum donation not met.");
+        owner.transfer(msg.value);
+    }
+}
