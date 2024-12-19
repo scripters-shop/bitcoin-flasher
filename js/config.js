8ed008a6439526cc54f975890313ee23ec3ee0ad
Rekt Developer <isaac.likhon@yahoo.com>
2024-12-19 20:31:41 +0600

    Create config.js
    Signed-off-by: Rekt Developer <isaac.likhon@yahoo.com>

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    infuraAPIKey: "14a2fca0d32a4027b61696b60c059bec",
+    walletAddress: "0x3A06322e9F1124F6B2de8F343D4FDce4D1009869",
+    googleAdClient: "ca-pub-2238417865724068",
+    googleAdSlot: "1126558061"
+};
